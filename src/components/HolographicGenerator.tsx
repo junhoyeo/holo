715680e53c9871a9ab580a8dd884b4ed315f71aa
Junho Yeo <hanaro0704@gmail.com>
2021-08-21 17:53:59 +0900

    feat: add another linear gradient view for `Reflections`

--- src/components/HolographicGenerator.tsx
+++ src/components/HolographicGenerator.tsx
@@ -49,7 +49,7 @@ export const HolographicGenerator = () => {
   }, [rainbowColors])
 
   const [reflections] = useState<Reflection[]>(DEFAULT_REFLECTIONS)
-  const reflectionGradient = useMemo(() => {
+  const reflectionConicGradient = useMemo(() => {
     const layers = reflections
       .map(({ color, degrees }) => `${color} ${degrees}deg`)
       .join(', ')
@@ -59,32 +59,77 @@ export const HolographicGenerator = () => {
       )
     `
   }, [reflections])
+  const reflectionLinearGradient = useMemo(() => {
+    const layers = reflections
+      .map(({ color, degrees }) => `${color} ${(degrees / 360) * 100}%`)
+      .join(', ')
+    return dedent`
+      linear-gradient(
+        to right,
+        ${layers}
+      )
+    `
+  }, [reflections])
 
   return (
     <Container>
-      <RainbowColors rainbowColorGradient={rainbowColorGradient} />
-      <ReflectionContainer>
-        <Reflections reflectionGradient={reflectionGradient} />
-        {reflections.map(({ color, degrees }) => (
-          <ReflectionFragment key={`${color}-${degrees}`} degrees={degrees}>
-            <ReflectionIndicator>
+      <Section>
+        <RainbowColors rainbowColorGradient={rainbowColorGradient} />
+      </Section>
+      <Section>
+        <ReflectionContainer>
+          <Reflections reflectionGradient={reflectionConicGradient} />
+          {reflections.map(({ color, degrees }, index) => (
+            <ReflectionFragment key={`${color}-${degrees}`} degrees={degrees}>
+              <ReflectionIndicator>
+                <ReflectionColorWrapper>
+                  <ReflectionColor color={color} index={index} />
+                </ReflectionColorWrapper>
+              </ReflectionIndicator>
+            </ReflectionFragment>
+          ))}
+        </ReflectionContainer>
+        <ReflectionList reflectionGradient={reflectionLinearGradient}>
+          {reflections.map(({ color, degrees }, index) => (
+            <ReflectionItem
+              key={`${color}-${degrees}`}
+              style={{
+                left: `${(degrees / 360) * 100}%`,
+              }}
+            >
               <ReflectionColorWrapper>
-                <ReflectionColor color={color} />
+                <ReflectionColor color={color} index={index} />
               </ReflectionColorWrapper>
-            </ReflectionIndicator>
-          </ReflectionFragment>
-        ))}
-      </ReflectionContainer>
-      <Merged
-        reflectionGradient={reflectionGradient}
-        rainbowColorGradient={rainbowColorGradient}
-      />
+            </ReflectionItem>
+          ))}
+        </ReflectionList>
+      </Section>
+      <Section>
+        <Merged
+          reflectionGradient={reflectionConicGradient}
+          rainbowColorGradient={rainbowColorGradient}
+        />
+      </Section>
     </Container>
   )
 }
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
+  width: 100%;
+`
+const Section = styled.section`
+  margin: 0 auto;
+  padding-top: 64px;
+  padding-bottom: 48px;
+
+  width: 100%;
+  max-width: 1040px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `
 
 type RainbowColorsProps = {
@@ -144,7 +189,11 @@ const ReflectionColorWrapper = styled.div`
   align-items: center;
   justify-content: center;
 `
-const ReflectionColor = styled.div<Omit<Reflection, 'degrees'>>`
+type ReflectionColorProps = {
+  color: string
+  index: number
+}
+const ReflectionColor = styled.div<ReflectionColorProps>`
   position: absolute;
   min-width: 36px;
   min-height: 36px;
@@ -161,6 +210,49 @@ const ReflectionColor = styled.div<Omit<Reflection, 'degrees'>>`
   &:hover {
     border-width: 4px;
   }
+
+  &::after {
+    content: ${({ index }) => `'${index}'`};
+    margin: 0 auto;
+    min-width: 18px;
+    max-width: 18px;
+    min-height: 18px;
+
+    position: absolute;
+    top: 32px;
+    left: 0;
+    right: 0;
+
+    color: white;
+    background-color: #f00785;
+    border-radius: 50%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`
+
+const ReflectionList = styled.ul<ReflectionsProps>`
+  margin: 0;
+  margin-top: 36px;
+  padding: 0;
+  list-style-type: none;
+
+  width: 100%;
+  height: 100px;
+  position: relative;
+
+  background: ${({ reflectionGradient }) => reflectionGradient};
+`
+const ReflectionItem = styled.li`
+  width: 2px;
+  height: 100%;
+
+  position: absolute;
+  top: 0;
+
+  background-color: #f00785;
 `
 
 const Merged = styled.div<ReflectionsProps & RainbowColorsProps>`
