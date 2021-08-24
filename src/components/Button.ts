2e03aa9f54e1aeac3c2c14dfd1a442f9049d9ee7
Junho Yeo <hanaro0704@gmail.com>
2021-08-25 04:03:30 +0900

    feat: add transition when `Button` is hovered/active

--- src/components/Button.ts
+++ src/components/Button.ts
@@ -5,10 +5,20 @@ export const Button = styled.button`
   padding: 16px 24px;
   cursor: pointer;
 
-  border-radius: 4px;
+  border-radius: 8px;
   border: 1px solid #333;
   background-color: white;
 
   font-size: 16px;
   font-weight: bold;
+
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+
+  &:active {
+    background-color: rgba(255, 255, 255, 0.85);
+  }
 `
